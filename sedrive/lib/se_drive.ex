1095a729e0786a1211c55a79247c7d8ba9e786cb
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-14 19:28:23 -0400

    `mix new`

---
+++ sedrive/lib/se_drive.ex
@@ -0,0 +1,18 @@
+defmodule SEDrive do
+  @moduledoc """
+  Documentation for `SEDrive`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SEDrive.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ sedrive/test/se_drive_test.exs
@@ -0,0 +1,8 @@
+defmodule SEDriveTest do
+  use ExUnit.Case
+  doctest SEDrive
+
+  test "greets the world" do
+    assert SEDrive.hello() == :world
+  end
+end
