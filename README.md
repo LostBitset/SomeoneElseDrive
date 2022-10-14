6111ec9bd7b76470792c38833f4e70c3a60869d2
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-14 19:35:32 -0400

    begin :)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SomeoneElseDrive
 Store your data (not) securely using random website caches!
+
+To use, run:
+```sh
+mix run -e 'SEDrive.interactive'
+```
+
--- sedrive/lib/se_drive.ex
+++ sedrive/lib/se_drive.ex
@@ -15,4 +15,12 @@ defmodule SEDrive do
   def hello do
     :world
   end
+
+  @doc """
+  Run an interactive session allowing you
+  to use SEDrive.
+  """
+  def interactive do
+    IO.puts "not yet implemented"
+  end
 end
