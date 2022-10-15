bf724f45ec636f5956d15b4a3cb7f83e9e59756d
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-14 20:44:42 -0400

    more progress

--- sedrive/lib/Controller.ex
+++ sedrive/lib/Controller.ex
@@ -5,7 +5,9 @@ defmodule Controller do
   """
 
   def start_link do
-    Task.start_link(fn -> loop(%{}) end)
+    Task.start_link(fn ->
+      loop(start_comms(), %{})
+    end)
   end
 
   def block_size, do: 8
@@ -14,11 +16,25 @@ defmodule Controller do
     block_size() * block_count()
   end
 
-  @doc """
-  The main loop for the process.
-  The state is queued instructions (the map keys are blocks)
-  """
-  @spec loop(%{integer => [SEDrive.Instr.t]}) :: no_return
-  defp loop(queued) do
+  @type instr :: {:get, integer} | {:set, integer, boolean}
+
+  @spec loop(pid, %{integer => [instr]}) :: no_return
+  defp loop(comms, queued) do
     receive do
+      {:instr, instr} ->
+        {block, block_instr} = make_block_instr(instr)
+        loop(comms, %{queued | block => block_instr})
+      {:refresh_block, block} ->
+        buf = read_block(comms, block)
+        buf = apply_instrs(queued[block], buf)
+        write_block(comms, block, buf)
+        queued(comms, Map.delete(queued, block))
+      :refresh ->
+        if block_count() != 0 do
+          send self(), {:refresh_block, 0}
+        end
+        loop(comms, queued)
+    end
+  end
+end
 
