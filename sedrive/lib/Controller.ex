57b346368ea3dd489f60913ab93486aed032deed
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-16 21:21:28 -0400

    just a bit more

--- sedrive/lib/Controller.ex
+++ sedrive/lib/Controller.ex
@@ -21,6 +21,15 @@ defmodule SEDrive.Controller do
   def handle_cast({:write, file, contents}, state) do
     {:noreply, cons_instr(state, file, {:write, contents})}
   end
+  def handle_cast({:refresh, file, next_period}, state) do
+    if Map.has_key?(state, file) do
+      refresh_file(file, state[file], next_period)
+      {:noreply, Map.delete(state, file)}
+    else
+      refresh_file(file, [], next_period)
+      {:noreply, state}
+    end
+  end
 
   @spec cons_instr(state, String.t, instr) :: state
   defp cons_instr(state, file, instr) do
@@ -32,5 +41,9 @@ defmodule SEDrive.Controller do
       end
     %{state | file => [instr | tail]}
   end
-end
 
+  @spec refresh_file(String.t, [instr], integer) :: nil
+  def refresh_file(file, instrs, integer) do
+    "not yet defined"
+  end
+end
