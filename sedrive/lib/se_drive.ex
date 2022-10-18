62aa8828e04ecca4d07cdf98b22c627da0f84ca4
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-18 14:53:38 -0400

    YYAYAYYAYAYYAY

--- sedrive/lib/se_drive.ex
+++ sedrive/lib/se_drive.ex
@@ -22,12 +22,14 @@ defmodule SEDrive do
   def main do
     cache = SEDrive.Conn.Sources.toyota
     SEDrive.Conn.Supervisor.start_link([])
+    loc = ["xfakesto=blahtest56", "time=#{System.monotonic_time()}"]
+    IO.puts "Using location: #{cache.url.(loc)}"
     num = IO.gets "Enter a number: "
     {num, "\n"} = Integer.parse(num)
-    {width, loc} = {8, ["xfakesto=blahtest56", "time=#{System.monotonic_time()}"]}
-    IO.puts "Storing at location: #{cache.url.(loc)}"
-    SEDrive.Conn.Supervisor.write_integer(cache, loc, num, width)
-    res = SEDrive.Conn.Supervisor.read_destroy_integer(cache, loc, width)
+    IO.puts "Writing..."
+    SEDrive.Conn.Supervisor.write_dyn(cache, loc, num)
+    IO.puts "Reading..."
+    res = SEDrive.Conn.Supervisor.read_destroy_dyn(cache, loc)
     IO.puts "Decoded from cache: #{inspect res}"
   end
 end
