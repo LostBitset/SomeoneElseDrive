42cd45d5443c64ec77e304a7020bd1aa5b605bae
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-17 21:26:07 -0400

    NUMBERS WORK

--- sedrive/lib/se_drive.ex
+++ sedrive/lib/se_drive.ex
@@ -15,5 +15,20 @@ defmodule SEDrive do
   def hello do
     :world
   end
+
+  @doc """
+  Just a test for now.
+  """
+  def main do
+    cache = SEDrive.Conn.Sources.toyota
+    SEDrive.Conn.Supervisor.start_link([])
+    num = IO.gets "Enter a number: "
+    {num, "\n"} = Integer.parse(num)
+    {width, loc} = {8, ["xfakesto=blahtest56", "time=#{System.monotonic_time()}"]}
+    IO.puts "Storing at location: #{cache.url.(loc)}"
+    SEDrive.Conn.Supervisor.write_integer(cache, loc, num, width)
+    res = SEDrive.Conn.Supervisor.read_destroy_integer(cache, loc, width)
+    IO.puts "Decoded from cache: #{inspect res}"
+  end
 end
 
