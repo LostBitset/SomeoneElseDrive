1f8007a817591161f49a7e3e5188974ebdc24466
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-18 16:30:52 -0400

    IT WORKY!!!?!?!?!!

--- sedrive/lib/se_drive.ex
+++ sedrive/lib/se_drive.ex
@@ -20,16 +20,36 @@ defmodule SEDrive do
   Just a test for now.
   """
   def main do
-    cache = SEDrive.Conn.Sources.toyota
+    {cache, loc} = setup()
+    SEDrive.Refresh.Supervisor.start_link([])
+    IO.puts "Creating..."
+    SEDrive.Refresh.Supervisor.create_new(cache, loc, "<initial contents>")
+    IO.puts "Waiting..."
+    :timer.sleep(6000)
+    IO.puts "Reading..."
+    res = SEDrive.Refresh.Supervisor.refresh(cache, loc)
+    IO.puts "Decoded from cache: #{inspect res}"
+  end
+
+  @doc """
+  A simple test for storing strings.
+  """
+  def test_strings do
+    {cache, loc} = setup()
     SEDrive.Conn.Supervisor.start_link([])
-    loc = ["xfakesto=blahtest56", "time=#{System.monotonic_time()}"]
-    IO.puts "Using location: #{cache.url.(loc)}"
     str = IO.gets "Enter some text: "
     IO.puts "Writing..."
     SEDrive.Conn.Supervisor.write_string(cache, loc, str)
     IO.puts "Reading..."
     res = SEDrive.Conn.Supervisor.read_destroy_string(cache, loc)
     IO.puts "Decoded from cache: #{res}"
+  end
+
+  defp setup do
+    cache = SEDrive.Conn.Sources.toyota
+    loc = ["xfakesto=blahtest56", "time=#{System.monotonic_time()}"]
+    IO.puts "Using location: #{cache.url.(loc)}"
+    {cache, loc}
   end
 end
 
