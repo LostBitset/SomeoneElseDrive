b6b57e0f9616ae00012be1b0a018b93e914c1959
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-17 21:08:30 -0400

    add toyota

---
+++ sedrive/lib/conn/sources.ex
@@ -0,0 +1,18 @@
+defmodule SEDrive.Conn.Sources do
+  @moduledoc """
+  Some caches that can be used to store data.
+  The only public members of this module should be of type
+  SEDrive.Conn.Cache.t (aliased as Cache), and should be spec'd as such.
+  """
+  alias SEDrive.Conn.Cache, as: Cache
+
+  @spec toyota :: Cache.t
+  def toyota do
+    Cache.from_single_header(
+      "https://www.toyota.com/etc.clientlibs/tcom/clientlibs/clientlib-carouselcontainerv2.min.d32f29c0e74277cda235ee0fdb49af7c.js",
+      "x-cache",
+      "Hit from cloudfront"
+    )
+  end
+end
+
